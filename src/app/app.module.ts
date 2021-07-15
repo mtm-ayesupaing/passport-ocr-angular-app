c373dfe518aabb7201a759d439530a2ce46921f2
Tin Myat Win <scm.dott.developer6@gmail.com>
2021-07-15 14:04:30 +0630

    Issue#1(bind ocr data)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { UserListComponent } from './components/user-list/user-list.component';
 import { UserAddComponent } from './components/user-add/user-add.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { ImageUploadComponent } from './components/image-upload/image-upload.component';
-import { PassportAddComponent } from './components/passport-add/passport-add.component';
+import { PassportAddComponent } from './dialogs/passport-add/passport-add.component';
 import { PassportListComponent } from './components/passport-list/passport-list.component';
 
 @NgModule({
--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiMessage } from 'src/app/constants/apiMessage';
+import { UploadImageService } from 'src/app/services/upload-image.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { MatDialog } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
+import { PassportModelService } from 'src/app/service-models/passport-model.service';
+import { ActivatedRoute, Router} from '@angular/router';
+import { PassportAddComponent } from 'src/app/dialogs/passport-add/passport-add.component';
 
 @Component({
   selector: 'app-image-upload',
@@ -10,33 +20,72 @@ export class ImageUploadComponent implements OnInit {
   public formData: FormData = new FormData();
   public file: any;
   public url: any;
-  public converted_data = "";
-
+  public convertedData = "";
+  public passportData : any;
+  @ViewChild('imageInput') imageInput: any;
+  @ViewChild('fileInput') fileInput: ElementRef | undefined;
+  public isGoodResolution: boolean = false;
+  public imagePath: any;
+  public message = '';
   constructor(
     private userSvc: UserService,
+    private uploadImgSvc: UploadImageService,
+    private apiMsg: ApiMessage,
+    private snackBarSvc: SnackbarService,
+    private dialog: MatDialog,
+    public formBuilder: FormBuilder,
+    public router: Router,
+    public passportModelSvc: PassportModelService,
   ) { }
 
   ngOnInit(): void {
   }
 
   onChangeImage(event: any) {
-    this.file = event.target.files[0];
-    this.formData.append('file', this.file, this.file.name);    
-    var reader = new FileReader();
-		reader.readAsDataURL(this.file);
-		
-		reader.onload = (_event) => {
-			this.url = reader.result; 
-		}
+    const URL = window.URL || window.webkitURL;
+    const Img = new Image();
+    const filesToUpload = (event.target.files);
+    const file = filesToUpload[0] as HTMLInputElement;    
+    Img.src = URL.createObjectURL(filesToUpload[0]);    
+    Img.onload = (e: any) => {
+      const height = e.path[0].height;
+      const width = e.path[0].width;
+      if ( width < 1000 && height < 900) {
+        this.snackBarSvc.open('full resolution is not included in Your Uploaded File ', 5000);
+        this.isGoodResolution = false;
+        this.url = '';
+      } else {
+        this.isGoodResolution = true;
+        var reader = new FileReader();
+        this.imagePath = event.target.files;
+        reader.readAsDataURL(event.target.files[0]); 
+        reader.onload = (_event) => { 
+          this.url = reader.result; 
+        }
+        this.formData.append('file', event.target.files[0], event.target.files[0].name);
+      }
+    }
   }
 
-  uploadImage(): void {
-    console.log("FILE :: ", this.file);
-    this.userSvc.uploadImage(this.formData).subscribe((data) => {
-      console.log("Image Data :: ", data);
-      this.converted_data = data;
-    }, error => {
-      console.log('ERROR :: ', error);
+  async uploadImage(): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      this.uploadImgSvc.uploadImage(this.formData).subscribe(
+        (res: any) => {
+          console.log(res);
+          this.convertedData = res.raw_data;
+          this.passportModelSvc.passportData = res;
+        },
+        (err: any) => {
+        })
+    });
+  }
+
+  bindPassportData(): void {
+    const dialogRef = this.dialog.open(PassportAddComponent, {
+      width: '900px',
+    });
+    dialogRef.afterClosed().subscribe(() => {
+      
     });
   }
 }
